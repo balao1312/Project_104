e3408e8abc9870cb9d24e3f8baa689f00b856b0e
balao1312 <balao1014@hotmail.com>
2020-07-19 22:00:42 +0800

    11

--- firstapp.py
+++ firstapp.py
@@ -70,4 +70,4 @@ def start_here():
                                                      # 導向結果網頁，把需要的變數一併傳出
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port = 5000)
